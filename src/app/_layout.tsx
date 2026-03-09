7d6861eb79287e77dad33087fb74f5570a66336c
“kikiolaoluwa <adeniran.kikiolaoluwa0409@gmail.com>
2026-03-09 01:15:15 +0100

    added splash screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,24 @@
 import { Slot } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+import SplashScreen from "@/components/SplashScreen";
 import "../../global.css"
 
 export default function Rootlayout() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
