9e7dd8a39d4809a1ea7fd94d456c785997a31a6f
“kikiolaoluwa <adeniran.kikiolaoluwa0409@gmail.com>
2026-03-09 12:03:30 +0100

    added a link

--- src/app/(auth)/onboarding.tsx
+++ src/app/(auth)/onboarding.tsx
@@ -1,13 +1,17 @@
-import { View, Text} from 'react-native'
-import OnboardingCarousel from '@/components/onboardingCarousel'
-import React from 'react'
-
+import { View, Text } from "react-native";
+import OnboardingCarousel from "@/components/onboardingCarousel";
+import React from "react";
+import { Link } from "expo-router";
 
 export default function onboarding() {
   return (
-    <View className='flex-1'>
-      <Text className="self-end mr-4 text-right text-lg font-semibold">skip</Text>
-      <OnboardingCarousel/>
+    <View className="flex-1">
+      <Link className="self-end mr-4 text-right text-lg font-semibold" href="/(auth)/chooseRole">
+        <Text >
+          skip
+        </Text>
+      </Link>
+      <OnboardingCarousel />
     </View>
-  )
+  );
 }
