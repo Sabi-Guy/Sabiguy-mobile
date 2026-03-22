325485079da34a667546c6c0960806278c3bee68
TurahRae <Turahrae@gmail.com>
2026-03-22 20:34:07 +0100

    Remove work coverage from personal info flow

--- src/app/(auth)/(serviceProvider)/login.tsx
+++ src/app/(auth)/(serviceProvider)/login.tsx
@@ -60,7 +60,7 @@ export default function ServiceProviderLogin() {
         type: "success",
         text1: "Login successful",
       });
-      router.push("/(protected)/(tabs)");
+      router.push("/(auth)/(serviceProvider)/personal-information");
     } catch (err) {
       Toast.show({
         type: "error",
