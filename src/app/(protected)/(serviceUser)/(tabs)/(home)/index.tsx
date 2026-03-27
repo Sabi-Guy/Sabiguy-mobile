f1725498bd46fc58aefe0a71d7ee5f5fcdd854a0
“kikiolaoluwa <adeniran.kikiolaoluwa0409@gmail.com>
2026-03-27 15:19:43 +0100

    fixed the background

--- src/app/(protected)/(serviceUser)/(tabs)/(home)/index.tsx
+++ src/app/(protected)/(serviceUser)/(tabs)/(home)/index.tsx
@@ -1,17 +1,24 @@
 import { Text, View, ScrollView, Image } from "react-native";
 import bell from "../../../../../../assets/bell.png";
 import location from "../../../../../../assets/location.png";
+import bgimage from "../../../../../../assets/bgimage.png";
 import SearchBar from "../../../../../components/SearchBar";
 
 export default function App() {
   return (
     <ScrollView>
       {/* top view */}
-      <View className="bg-[#2E7D52] px-5 pt-14 pb-6 rounded-b-3xl">
+      <View className="bg-[#2E7D52] px-5 pt-12 pb-6 rounded-b-3xl overflow-hidden">
+        <Image
+          source={bgimage}
+          className="absolute bottom-0 h-24 opacity-40"
+          style={{ left: 0, right: 0, width: "100%" }}
+          resizeMode="cover"
+        />
         {/* top */}
         <View className="flex-row items-start justify-between">
           <View className="flex-1 pr-4">
-            <Text className="text-white text-lg font-semibold">Hello, Kuks</Text>
+            <Text className="text-white text-lg font-semibold">Hello, Kuks 👋</Text>
             <View className="mt-2 flex-row items-center py-1.5 rounded-full self-start">
               <Image source={location} className="h-5 w-5"/>
               <Text className="text-white text-xs"> 24, Ipaja, Lagos, Nigeria </Text>
@@ -30,7 +37,20 @@ export default function App() {
       </View>
 
       {/* main content */}
-      <View></View>
+      <View className="m-5 ">
+        {/* popular */}
+        <View>
+          <Text>Popular Services</Text>
+        </View>
+        {/* categories */}
+        <View>
+          <Text>Categories</Text>
+        </View>
+        {/* popular providers */}
+        <View>
+          <Text>Popular Providers</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 }
